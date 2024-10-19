5b27a1a9f184e4ba9dd44a5bd12a97aaf7d34b93
Yicheng Wang <Richardwang42.42@gmail.com>
2024-10-19 14:07:53 -0700

    lints + proof structures

--- trueCanvasAPI/go-app/main.go
+++ trueCanvasAPI/go-app/main.go
@@ -13,6 +13,33 @@ import (
 
 const proverURL string = "https://example.com"
 
+// Proof Structures
+type ZKProof struct {
+	SourceHash string   `json:"sourceHash"`
+	DestHash   string   `json:"destHash"`
+	Proof      []string `json:"proof"`
+}
+
+type GetProofResponse struct {
+	ProofStatus string  `json:"proofStatus"`
+	ZKproof     ZKProof `json:"ZKproof"`
+}
+
+// Helpers
+func allowAllOrigns(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+}
+
+func hashString(input string) string {
+	hash := sha256.New()
+	hash.Write([]byte(input))
+	hashBytes := hash.Sum(nil)
+	return hex.EncodeToString(hashBytes)
+}
+
+// Verification Helpers
 type VerifyRequest struct {
 	Image string `json:"image"`
 	Logs  string `json:"logs"`
@@ -23,42 +50,10 @@ type VerifyResponse struct {
 	ImageID            string `json:imageID`
 }
 
-func allowAllOrigns(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-}
-
-// HashString takes a string and returns its SHA-256 hash as a hexadecimal string.
-func hashString(input string) string {
-	hash := sha256.New()
-	hash.Write([]byte(input))
-	hashBytes := hash.Sum(nil)
-	return hex.EncodeToString(hashBytes)
-}
-
-// indexHandler responds to requests with our greeting.
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	allowAllOrigns(w)
-	fmt.Fprint(w, "Hello, World!")
-}
-
-func getProofStatusHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/proof_status" {
-		http.NotFound(w, r)
-		return
-	}
-	allowAllOrigns(w)
-	fmt.Fprint(w, "Proof Status")
-}
-
 func verifyLogsAsLegitimate(logsText string) bool {
 	pasteCount := countWordOccurrences(logsText, "paste")
 
+	// Check if paste comprises of 25% of logs... this can be made better.
 	if (pasteCount*5)/4 >= len(logsText) {
 		return false
 	}
@@ -69,13 +64,10 @@ func countWordOccurrences(text, word string) int {
 	wordLen := len(word)
 	count := 0
 
-	// Normalize the text and word for case-insensitive comparison
 	text = strings.ToLower(text)
 	word = strings.ToLower(word)
 
-	// Sliding window approach
 	for i := 0; i <= len(text)-wordLen; i++ {
-		// Extract the substring of the same length as the word
 		substr := text[i : i+wordLen]
 		if substr == word {
 			count++
@@ -85,6 +77,41 @@ func countWordOccurrences(text, word string) int {
 	return count
 }
 
+// Miscellaneous
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	allowAllOrigns(w)
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// Async To Get Verify
+func callProver() {
+	res, err := http.Get(proverURL)
+	if err != nil {
+		log.Printf((err.Error()))
+	}
+	log.Printf("End line has been reached %d", res.StatusCode)
+}
+
+// GetProof Status
+func getProofStatusHandler(w http.ResponseWriter, r *http.Request) {
+	allowAllOrigns(w)
+	if r.Method != http.MethodGet {
+		return
+	}
+	if r.URL.Path != "/proof_status" {
+		http.NotFound(w, r)
+		return
+	}
+
+	imageID := r.URL.Query().Get("imageID")
+	fmt.Fprint(w, imageID)
+}
+
+// PostVerify Request
 func postVerify(w http.ResponseWriter, r *http.Request) {
 	allowAllOrigns(w)
 	if r.Method != http.MethodPost {
@@ -134,15 +161,6 @@ func getAvailableContracts(w http.ResponseWriter, r *http.Request) {
 	}
 	allowAllOrigns(w)
 	fmt.Fprint(w, "Available Contracts")
-}
-
-// Async To Get Verify
-func callProver() {
-	res, err := http.Get(proverURL)
-	if err != nil {
-		log.Printf((err.Error()))
-	}
-	log.Printf("End line has been reached %d", res.StatusCode)
 }
 
 func main() {
